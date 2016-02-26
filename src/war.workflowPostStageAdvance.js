450922b309ade4613ecaf82213b83b5c0271e2db
Bjorn Westergard <bjornw@gmail.com>
2016-02-25 19:39:24 -0500

    updated for local testing
    Signed-off-by: Bjorn Westergard <bjornw@gmail.com>

--- src/war.workflowPostStageAdvance.js
+++ src/war.workflowPostStageAdvance.js
@@ -4,11 +4,14 @@ export function handler (event, { succeed, fail }) {
   const { contents: { recordId, recordType, toStage } } = event
   const request = API(event)
   const done = (e, res) => e ? fail(e) : succeed(res)
+
+  if (event.content.workflow !== 'fight_war') done(null, 'irrelevant workflow')
+
   const chance_to_win = ({
-    third: 0.25,
-    forth: 0.50,
-    fifth: 0.75,
-    sixth: 1.00
+    arm: 0.25,
+    fight: 0.50,
+    assess_damage: 0.75,
+    fight_war_complete: 1.00
   })[toStage]
 
   request({
