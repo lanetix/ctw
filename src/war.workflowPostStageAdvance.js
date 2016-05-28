303afad957f03ea7c846bc2f65ecafb1f04559db
Bjorn Westergard <bwestergard@Bjorns-MBP-2.home>
2016-05-28 19:56:17 -0400

    host of changes
    Signed-off-by: Bjorn Westergard <bwestergard@Bjorns-MBP-2.home>

--- src/war.workflowPostStageAdvance.js
+++ src/war.workflowPostStageAdvance.js
@@ -23,20 +23,22 @@ import API from '../lib/api'
 export function handler (event, { succeed, fail }) {
   console.log(`event: ${JSON.stringify(event, null, 2)}`)
 
-  // recordId, recordType, toStage, workflow
-  //const { lxMessage: { message: { contents: { recordId, recordType, toStage, workflow } } } } = event
-  const { payload: { record: { id, apiName }, fromStage, toStage } } = event
+  const recordId = event.payload.record.id
+  const recordType = event.payload.record.apiName
+  const workflowName = event.payload.workflow.apiName
+
+  const { payload: { fromStage, toStage } } = event
   const request = API(event) // Extracts JWT from event, returns authenticated request function
   const done = (e, res) => e ? fail(e) : succeed(res)
 
-  if (workflow.name !== 'sure_start') done(null, 'irrelevant workflow')
+  if (workflowName !== 'sure_start') done(null, 'irrelevant workflow')
 
   const chance_to_win = ({
     'prospect': 0,
     'qualified': 25,
     'proposed': 50,
     'contract': 75,
-    'go-live': 100,
+    'go_live': 100,
     'stabilization': 0
   })[toStage]
 
@@ -44,7 +46,7 @@ export function handler (event, { succeed, fail }) {
 
   request({
     method: 'PATCH',
-    path: `/v1/records/${apiName}/${id}`,
+    path: `/v1/records/${recordType}/${recordId}`,
     body: { chance_to_win }
   }, function (err, res) {
     console.log(`${err}, ${res}`)
